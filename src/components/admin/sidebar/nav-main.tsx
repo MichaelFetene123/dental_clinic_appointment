2bb2b7adc21d58d618c9772361404d6135db0981
MichaelFetene123 <michaelfetene0202@gmail.com>
2026-07-08 11:49:07 +0300

    feat: add NavMain component for admin sidebar navigation

--- src/components/admin/sidebar/nav-main.tsx
+++ src/components/admin/sidebar/nav-main.tsx
@@ -25,14 +25,14 @@ export function NavMain({
     return (
         <SidebarGroup>
             <SidebarGroupContent className="flex flex-col gap-2">
-                {name && <SidebarGroupLabel><span className="text-[16px]">{name}</span></SidebarGroupLabel>}
+                {name && <SidebarGroupLabel className="group-data-[collapsible=icon]:hidden"><span className="text-[16px]">{name}</span></SidebarGroupLabel>}
                 <SidebarMenu>
                     {items.map((item) => (
                         <SidebarMenuItem key={item.title} className="py-2">
-                            <SidebarMenuButton tooltip={item.title} className="py-3">
-                                <Link href={item.url.startsWith('/admin') ? item.url : `/admin${item.url}`} className="flex items-center gap-2 py-3">
+                            <SidebarMenuButton tooltip={item.title} className="py-3 transition-all duration-200 group-data-[collapsible=icon]:!p-0 group-data-[collapsible=icon]:justify-center">
+                                <Link href={item.url.startsWith('/admin') ? item.url : `/admin${item.url}`} className="flex items-center gap-2 py-3 group-data-[collapsible=icon]:py-0 group-data-[collapsible=icon]:w-full group-data-[collapsible=icon]:h-full group-data-[collapsible=icon]:justify-center">
                                     {item.icon && <item.icon size={24} className=""/>}
-                                    <span className="text-[16px]">{item.title}</span>
+                                    <span className="text-[16px] group-data-[collapsible=icon]:hidden">{item.title}</span>
                                 </Link>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
